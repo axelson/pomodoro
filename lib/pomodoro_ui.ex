6a050da4343c5edf1cfb3a1c4c8b7ac627bd0226
Jason Axelson <jason.axelson@gmail.com>
2019-09-26 08:03:13 -1000

    Only start scenic if viewoport is configured
    This allows this to be used as a "library"

--- lib/pomodoro_ui.ex
+++ lib/pomodoro_ui.ex
@@ -5,12 +5,20 @@ defmodule PomodoroUi do
   use Boundary, deps: [Pomodoro, ScenicUtils], exports: []
 
   def start(_type, _args) do
-    main_viewport_config = Application.get_env(:pomodoro, :viewport)
-
-    children = [
-      {Scenic, viewports: [main_viewport_config]}
-    ]
+    children =
+      []
+      |> maybe_add_scenic_child()
 
     Supervisor.start_link(children, strategy: :one_for_one)
+  end
+
+  defp maybe_add_scenic_child(children) do
+    main_viewport_config = Application.get_env(:pomodoro, :viewport)
+
+    if main_viewport_config do
+      [{Scenic, viewports: [main_viewport_config]} | children]
+    else
+      children
+    end
   end
 end
