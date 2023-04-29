3c09dc803721082eb2d51e96c70bf4c40e212250
Jason Axelson <jason.axelson@gmail.com>
2023-04-29 09:58:46 -1000

    deps update

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Pomodoro.MixProject do
       {:ecto_sqlite3, "~> 0.7"},
       {:exsync, path: "~/dev/forks/exsync", only: :dev, override: true},
       {:truetype_metrics, "~> 0.6"},
-      {:muontrap, "~> 0.6.1 or ~> 1.0"},
+      {:muontrap, "~> 0.6.1 or ~> 1.0"}
       # {:slack, "~> 0.19"}
     ]
   end
