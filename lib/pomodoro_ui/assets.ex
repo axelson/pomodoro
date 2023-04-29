8f0c7b5bc42a2848b98c2f018553bed8e74f2ea4
Jason Axelson <jason.axelson@gmail.com>
2023-04-29 10:29:06 -1000

    remove our custom __mix_recompile__? for assets
    This is built in to Scenic now

--- lib/pomodoro_ui/assets.ex
+++ lib/pomodoro_ui/assets.ex
@@ -1,17 +1,5 @@
 defmodule PomodoroUi.Assets do
-  @asset_path Path.join([__DIR__, "..", "..", "assets"]) |> Path.expand()
-  paths = Path.wildcard("#{@asset_path}/*")
-  paths_hash = :erlang.md5(paths)
-
-  for path <- paths do
-    @external_resource path
-  end
-
   use Scenic.Assets.Static, otp_app: :pomodoro
 
   def asset_path, do: Path.join([__DIR__, "..", "..", "assets"]) |> Path.expand()
-
-  def __mix_recompile__? do
-    Path.wildcard("#{asset_path()}/*") |> :erlang.md5() != unquote(paths_hash)
-  end
 end
