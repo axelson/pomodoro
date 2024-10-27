5abe009f816c303e67383f37d854f3c3a2aacc91
Jason Axelson <jason.axelson@gmail.com>
2024-10-27 11:10:32 -0700

    Tighten up starting the supervision tree

--- lib/pomodoro_ui/application.ex
+++ lib/pomodoro_ui/application.ex
@@ -2,10 +2,10 @@ defmodule PomodoroUi.Application do
   def start(_type, _args) do
     children =
       [
-        maybe_start_timer(),
+        List.wrap(maybe_start_timer()),
         Pomodoro.Repo,
         {Task.Supervisor, name: :pomodoro_task_supervisor},
-        maybe_start_scenic()
+        List.wrap(maybe_start_scenic())
       ]
       |> List.flatten()
 
@@ -13,22 +13,16 @@ defmodule PomodoroUi.Application do
   end
 
   defp maybe_start_scenic do
-    main_viewport_config = Application.get_env(:pomodoro, :viewport)
-
-    if main_viewport_config do
+    if main_viewport_config = Application.get_env(:pomodoro, :viewport) do
       [
         {Scenic, [main_viewport_config]}
       ]
-    else
-      []
     end
   end
 
   defp maybe_start_timer do
     if Application.get_env(:pomodoro, :viewport) do
       Pomodoro.PomodoroTimer
-    else
-      []
     end
   end
 end
