627c2ea8f7b00aae47f27942cdeb9ac1a1a440c5
Jason Axelson <jason.axelson@gmail.com>
2024-10-28 15:41:50 -1000

    Finish update to Application

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Pomodoro.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      mod: {PomodoroUi.Application, []},
+      mod: {Pomodoro.Application, []},
       extra_applications: [:logger]
     ]
   end
